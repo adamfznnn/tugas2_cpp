7e6493ea24605437b2574594705421741dcff0a3
Wijdan <wijdanbita@gmail.com>
2026-02-24 18:04:24 +0700

    update : cors agar bisa konek ke FE

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+// Import Package dan File
 const express = require("express");
 const sequelize = require("./config/database");
 const userRoutes = require("./routes/userRoutes");
 
+// Inisialisasi Express dan Cors
 const app = express();
+const cors = require("cors");
+
+// Cara 1. Izinkan Semua Origin (tidak disarankan untuk produksi)
+app.use(cors());
+
+// Cara 2. Izinkan Frontend tertentu (misal Vite di localhost:5173)
+// app.use(cors({
+//   origin: 'http://localhost:5173', // Alamat Vite kamu
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true // Jika butuh kirim cookie/session
+// }));
+
+// Middleware untuk parsing JSON
 app.use(express.json());
 
+// Route dasar untuk testing
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Best practice: version your APIs
-require("./schema/User");
-app.use("/api/v1/users", userRoutes);
+// Setting Routes
+require("./schema/User"); // Untuk generate Tabel User
+app.use("/api/v1/users", userRoutes); // Untuk setting routes user
 
-// Sync Database & Start Server
+// Sync Database dan Jalankan Server
 const port = process.env.PORT || 3000;
 sequelize.sync().then(() => {
   console.log("Database synced");
